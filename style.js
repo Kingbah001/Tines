927ca17d257cf3e1cb57c4281c98ceff7c8a8a54
Kingbah001 <abdhakeembukhari@gmail.com>
2023-06-17 17:04:18 +0100

    done with tines project

---
+++ style.js
@@ -0,0 +1,7 @@
+let hamburgerdiv = document.querySelector('.hamburger');
+let mobileLinks = document.querySelector('.mobile-link-holder');
+
+function dosomething(){
+    hamburgerdiv.classList.toggle('showburger');
+    mobileLinks.classList.toggle('show-mobile-link-holder')
+}
